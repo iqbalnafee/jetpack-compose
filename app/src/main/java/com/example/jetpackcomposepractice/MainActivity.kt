dd91c225b379af69045c3615006003af5befa8c4
Nafees Iqbal <iqbalnafee@gmail.com>
2025-04-21 11:52:48 +0200

    surface and modifier added

--- app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposepractice.ui.theme.JetpackComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,13 +36,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(color = Color.Yellow) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+                .padding(16.dp)
+                .background(Color.Magenta)
+        )
+    }
 }
 
-@Preview(fontScale = 1.5f)
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
